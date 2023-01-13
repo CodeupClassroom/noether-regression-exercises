aaa9c52e85f1cf5fea028d0e75d6025e80f24454
tasharivers <116823780+tasharivers@users.noreply.github.com>
2023-01-13 09:33:07 -0600

    add scaling exercises

---
+++ wrangle.py
@@ -0,0 +1,63 @@
+#wrangle.py
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from env import user, password, host
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def acquire_zillow():
+    '''
+    This function checks to see if zillow.csv already exists, 
+    if it does not, one is created
+    '''
+    #check to see if telco_churn.csv already exist
+    if os.path.isfile('zillow.csv'):
+        df = pd.read_csv('zillow.csv', index_col=0)
+    
+    else:
+
+        #creates new csv if one does not already exist
+        df = get_zillow_data()
+        df.to_csv('zillow.csv')
+
+    return df
+
+def prep_zillow(df):
+    '''
+    This function takes in the zillow df
+    then the data is cleaned and returned
+    '''
+    #change column names to be more readable
+    df = df.rename(columns={'bedroomcnt':'bedrooms','bathroomcnt':'bathrooms', 'calculatedfinishedsquarefeet':'sqft', 'taxvaluedollarcnt':'home_value', 'taxamount':'yearly_tax', 'yearbuilt':'year_built'})
+
+    #drop null values- at most there were 9000 nulls (this is only 0.5% of 2.1M)
+    df = df.dropna()
+
+    #drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
+
+
+def wrangle_zillow():
+    '''
+    This function uses the acquire and prepare functions
+    and returns the split/cleaned dataframe
+    '''
+    train, validate, test = prep_zillow(acquire_zillow())
+    
+    return train, validate, test
